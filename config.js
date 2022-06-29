f579a4e6852525d13f3b45b368b6773372be4dd1
Ming <cmyeumn21@gmail.com>
2022-06-29 09:51:31 +0200

    What-happens-if-Byte-and-Quantum-add-together-FREE-FLP

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'MiNG',
+    introDesc: `FLP at next page.Tape Next`,
+    btnIntro: 'Next',
+    title: 'Please subscribe for my Youtube Channel',
+    desc: 'Do you like my video ? ',
+    btnYes: 'Yessssss i really like it',
+    btnNo: 'No it so bad',
+    question:'Have a lots videos in Youtube so why you like my video ?',
+    btnReply: 'Send',
+    reply: 'Because you are amazingg',
+    mess: 'Ohhh thank you so muchhh',
+    messDesc: 'Here It is a FLP free just for you<3',
+    btnAccept: 'Thank!',
+    messLink: 'https://www.mediafire.com/file/zt1l03gcb36elyg/What_happens_if_Byte_and_Quantum_add_together.rar/file'
+}
